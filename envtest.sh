df47233e7c574cd69905878f07345f25dbcf01cc
dmattie <dmattie@stfx.ca>
2020-08-04 20:14:18 -0300

    Needed some fancy stuff.  Added enviro test

---
+++ envtest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Checking environment..."
+
+errcnt=0
+
+while read l; do
+  package=$(echo $l |cut -d '=' -f 1)
+  pkgtest=$(pip list |grep $package|wc -l)
+  if [ $pkgtest -eq "0" ]
+  then
+    echo "xxxxxx FAIL xxxxxx $package not found"
+    let "errcnt+=1"
+  fi
+done <requirements.txt
+
+yellow=$(tput setaf 3)
+green=$(tput setaf 2)
+normal=$(tput sgr0)
+if [ $errcnt -eq "0" ]
+then
+   echo "Everything looks hunky-dory.  You can run $green make $normal to get going."
+else
+   echo "Looks like you are missing some packages.  Try:$yellow pip install -r requirements.txt"
+fi
